f92c11f8bd003f5a164ab6a87a1d54e773e9f8cb
Ethan <62487806+ethanol20@users.noreply.github.com>
2020-06-18 18:37:47 -0400

    Update fxmanifest.lua

--- rp_yankton/fxmanifest.lua
+++ rp_yankton/fxmanifest.lua
@@ -5,6 +5,10 @@ author 'ethanol20#4434'
 description 'Allows for players to fly to and fro San Andreas to North Yankton! Join my Discord for support or script requests! https://discord.gg/SQXvn5a'
 version '1.0.0'
 
+dependencies {
+	'NativeUI',
+}
+
 client_scripts {
 	"@NativeUI/NativeUI.lua",
     'client/main.lua',
