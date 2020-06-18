e53b8ede459fc890b0051317ff5cf1e27221deb2
Ethan <62487806+ethanol20@users.noreply.github.com>
2020-06-18 00:11:50 -0400

    Add files via upload

---
+++ rp_yankton/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'ethanol20#4434'
+description 'Allows for players to fly to and fro San Andreas to North Yankton! Join my Discord for support or script requests! https://discord.gg/SQXvn5a'
+version '1.0.0'
+
+client_scripts {
+	"@NativeUI/NativeUI.lua",
+    'client/main.lua',
+    "client/menu.lua",
+}
